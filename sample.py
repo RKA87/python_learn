9865e36785f59be7aaed78a561d696b18ce7c8c2
RAKESH <rakeshkumar.akuluru@gmail.com>
2024-07-10 22:26:13 -0500

    updated

--- sample.py
+++ sample.py
@@ -169,7 +169,7 @@ l=[1,4,5,19,0, 23, -1, 45]
 min1=l[0]
 for i in range(len(l)):
      # If the other element is min than first element
-    if l[i] < min1: 
+    if l[i] < min1:
         min1 = l[i] #It will change
- 
+
 print("The smallest element in the list is ",min1)

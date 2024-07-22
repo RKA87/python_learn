e0627a332bf3484941f2206cdd26bcdb79f5ad8a
RAKESH <rakeshkumar.akuluru@gmail.com>
2024-07-21 21:09:19 -0500

    Dictionary Tasks and Examples

--- 07212024.py
+++ 07212024.py
@@ -2,4 +2,14 @@
 
 # Taks 1:
 # For the given employees give a salary hike of 10% and return a new dict. 
-# d= {“Ramesh” : 2500, “Rajesh”: 1500, “Aish”: 3000, “Gaur” : 2400}
+
+d= {"Ramesh" : 2500, "Rajesh": 1500, "Aish": 3000, "Gaur" : 2400}
+hike_dict={}
+
+for key,val in d.items():
+    increased_value= val+((val*10)/100)
+    hike_dict[key] = increased_value
+
+print("Original Dictionary:",d)
+print("After Hike:",hike_dict)
+

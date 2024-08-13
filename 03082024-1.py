6d3e435a6d1b67aee0781cc71860e5eb412853a8
Rakesh Akuluru <Rakesh.Akuluru@dfa.arkansas.gov>
2024-08-13 10:50:06 -0500

    updated OS module tasks

--- 03082024-1.py
+++ 03082024-1.py
@@ -55,9 +55,21 @@ folder_path="C:/Temp/"
 name="test"
 
 i=1
+x=folder_path+name
 while i<=5:
-    os.makedirs(folder_path+name)
+    os.makedirs(x)
     i+=1
+    x=x+name
+
+# case 2
+path="c:/Temp/"
+directory="test"
+
+current_path=path
+
+for i in range(1,6):
+    current_path=os.path.join(current_path,directory)
+    os.makedirs(current_path)
 
 '''
 Task 4:
@@ -65,12 +77,76 @@ You have a folder with various images.
 Write a python program to display the image indicated by the user input. Use pil package.
 
 '''
+from PIL import Image
+import os
 
+def display_image(file_path,image_no_input):
+    list_images=os.listdir(file_path)
 
+    # filtering JPEG images only
+    pics_list=list(filter(lambda image:image.endswith(".jpg"), list_images))
+    image_indicate_directory={}
 
+    for i in range(len(pics_list)):
+        image_indicate_directory[i]=pics_list[i]
 
+    try:
+        # Display Image (image_indicate_directory)
+        display_image=image_indicate_directory[image_no_input]
+        open_file=Image.open(file_path+display_image)
+        return (open_file.show())
+    # except (KeyError,AttributeError,NameError,TypeError,SyntaxError) as E:
+    #     return ("Please Enter Input Range is from 0 to 3 only")
+    except Exception as ErrorInfo:
+        return ("Error Info:",ErrorInfo, "Enter Input Range is from 0 to 3 only")
 
+file_path="C:/Temp/"
+image_no_input=1
+result=display_image(file_path,image_no_input)
+print(result)
 
+'''
+Task 5
+Accept some input from the user and write it to a text file
+'''
+# case 1
+text_input=input("Enter the input to save required data:")
 
+file_path="C:/Temp/test.txt"
+with open(file_path, 'w') as file:
+    file.write(text_input)
 
+# case 2 using writelines
 
+# Open a non-existent file in write mode
+fo = open("C:/Temp/new.txt", "w")
+print("Name of the file: ", fo.name)
+
+# Write a new text
+seq = ["Hello", '\n', "Welcome to Tutorialspoint"]
+line = fo.writelines(seq)
+
+print("Check the file to see the reflected changes")
+
+# Close opened file
+fo.close()
+
+'''
+Task 6
+read a tex file from folder and display the count of the word entered by the user in the file
+'''
+
+fo=open("c:/Temp/new.txt",'r')
+file_read=fo.read()
+
+# # I've got the output is in list after split from the text file words. In the second line when the split does it creates "Hello'\n'World" like this.
+# to overcome this we can use splitlines and get the lists output with elements
+
+output_lines=file_read.splitlines("") 
+output_words=[]
+for word in output_lines:
+    for each_word in (word.split(" ")):
+        output_words.append(each_word)
+
+for length in output_words:
+    print(f"count of '{length}' word:",len(length))
